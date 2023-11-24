7f6e97bdd98c1dc287344ba86d1d468cd7fdf656
eloisedelorme <eloise.galland@lilo.org>
2023-11-24 20:17:40 +0100

    changement on text when kids set their name

--- js/formName.js
+++ js/formName.js
@@ -6,17 +6,31 @@
 
 // ETAPE 1 : Récupèrer le nom de l'enfant et le stocker dans une variable.
 
-let kidName =""
+let kidName ="";
 
-const form = document.querySelector("#form")
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const data = new FormData(form)
-    kidName = data.get("search")
-    console.log(kidName)
-    
-})
+const start = () => {
+    const form = document.querySelector(".js-form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const data = new FormData(form);
+        kidName = data.get("search");
+        console.log(kidName);
+        talkKid(kidName);
+    });
+}
 
 // ETAPE 2 : Effacer le formulaire pour demander à l'enfant s'il veut nous suivre au village du Père Noël 
 
+const talkKid = (x) => {
+    
+    let truc = document.querySelector(".js-text1");
+    truc = truc.classList.add("hidden");
+    
+    let removeHidden = document.querySelector(".js-text2");
+    removeHidden.classList.remove("hidden");
+    removeHidden.textContent = `${kidName}, suis moi au pays du Père Noël !`;
+};
 
+
+//DEMARRAGE
+start();
